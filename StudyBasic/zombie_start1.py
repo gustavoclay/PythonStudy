56343847f6d06a2d6cc4732a761cd10872728d53
Gustavo Clay <gustavoclay@gmail.com>
2018-04-10 14:06:38 -0300

    Entrada de Dados Aula 08

---
+++ StudyBasic/zombie_start1.py
@@ -0,0 +1,17 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+
+>>> divida = 0
+>>> compra = 42
+>>> divida = divida + compra
+>>> compra 13
+SyntaxError: invalid syntax
+>>> compra = 13
+>>> divida = divida + compra
+>>> compra = 50
+>>> divida = divida + compra
+>>> divida
+105
+>>> #www.pythontutor.com
+>>> 
---
+++ StudyBasic/zombie_test2.py
@@ -0,0 +1,3 @@
+n1 = float(input ('Digite um numero: '))
+n2 = float(input ('Digite outro numero: '))
+print (n1 + n2)
