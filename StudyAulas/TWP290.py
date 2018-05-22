65ce41c9b55a8522e2e65ed07fb92d0e32b3c578
Gustavo Clay <gustavoclay@gmail.com>
2018-05-22 18:28:48 -0300

    Continuar a partir da aula TWP296, estamos evoluindo :)

---
+++ StudyAulas/TWP290.py
@@ -0,0 +1,25 @@
+#for each
+print ("FOR:")
+for letra in 'aeiou':
+    print (letra)
+
+#while
+print ("WHILE:")
+texto = 'aeiou'
+k = 0
+while k < len(texto):
+    letra = texto[k]
+    print (letra)
+    k += 1
+
+#for each
+for i in range(5):
+    print(i)
+
+#while
+lista = list(range(5))
+k = 0
+while k < len(lista):
+    i = lista[k]
+    print (i)
+    k += 1
