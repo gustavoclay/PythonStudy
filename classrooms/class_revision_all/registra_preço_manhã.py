6e14d6de279e507ad426e8f68ea03a601b8cbf54
Gustavo Clay <gustavoclay@gmail.com>
2018-06-17 17:27:04 -0300

    Encerrado revisão geral, verificar lista de exercicios

--- classrooms/class_revision_all/registra_preço_manhã.py
+++ classrooms/class_revision_all/registra_preço_manhã.py
@@ -3,21 +3,20 @@ def salva_transação(preço, cartão_crédito, descrição):
     file.write("%16s%07d%16s\n" %(cartão_crédito, preço * 100, descrição))
     file.close()
 
-itens   = ["Bauru", "X Salada", "Calafrango"] 
-preços  = [2.50, 3.0, 2.20] 
-rodando = True 
+itens = ["Bauru", "X Salada", "Calafrango"]
+preços = [2.50, 3.0, 2.20]
+rodando = True
 
-while rodando: 
-    opção = 1 
-    for escolha in itens: 
-        print(str(opção) + ". " + escolha) 
-        opção = opção + 1 
-    print(str(opção) + ". Finalizar") 
-    escolha = int(input("Escolha uma opção: ")) 
+while rodando:
+    opção = 1
+    for escolha in itens:
+        print(str(opção) + ". " + escolha)
+        opção = opção + 1
+    print(str(opção) + ". Finalizar")
+    escolha = int(input("Escolha uma opção: "))
     if escolha == opção:
         #escolheu a última opção Finalizar
-        rodando = False 
-    else: 
+        rodando = False
+    else:
         cartão = input("Número do cartão de crédito: ")
         salva_transação(preços[escolha - 1], cartão, itens[escolha - 1])
-        
--- classrooms/class_revision_all/registra_preço_tarde_noite_com_módulo.py
+++ classrooms/class_revision_all/registra_preço_tarde_noite_com_módulo.py
@@ -1,20 +1,19 @@
 from banco import *
 
-itens   = ["Esfiha", "Coxinha", "Pastel", "Pão de Queijo"] 
-preços  = [1.50, 2.20, 1.80, 1.20] 
-rodando = True 
+itens = ["Esfiha", "Coxinha", "Pastel", "Pão de Queijo"]
+preços = [1.50, 2.20, 1.80, 1.20]
+rodando = True
 
-while rodando: 
-    opção = 1 
-    for escolha in itens: 
-        print(str(opção) + ". " + escolha) 
-        opção = opção + 1 
-    print(str(opção) + ". Finalizar") 
-    escolha = int(input("Escolha uma opção: ")) 
+while rodando:
+    opção = 1
+    for escolha in itens:
+        print(str(opção) + ". " + escolha)
+        opção = opção + 1
+    print(str(opção) + ". Finalizar")
+    escolha = int(input("Escolha uma opção: "))
     if escolha == opção:
         #escolheu a última opção Finalizar
-        rodando = False 
-    else: 
+        rodando = False
+    else:
         cartão = input("Número do cartão de crédito: ")
         salva_transação(preços[escolha - 1], cartão, itens[escolha - 1])
-        
