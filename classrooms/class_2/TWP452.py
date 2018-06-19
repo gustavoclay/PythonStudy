029f09009e7990850aa39067374544dcce54dce9
Gustavo Clay <gustavoclay@gmail.com>
2018-06-18 22:37:19 -0300

    Capitulo encerrado, iniciar capitulo web

--- classrooms/class_2/TWP452.py
+++ classrooms/class_2/TWP452.py
@@ -1,14 +1,12 @@
 from tkinter import *
-
-def apertei_botao():
-    print('Apertei o Botão!')
-
-
 app = Tk()
-app.title('Teste de Botão!')
+app.title("Show de Calouros")
 app.geometry('300x100+200+100')
 
-b = Button(app, text='Aperte-me', width=10, command=apertei_botao)
-b.pack(side='top', padx=10, pady=10)
+b1 = Button(app, text='Certo', width=10)
+b1.pack(side='left', padx=10, pady=10)
+
+b2 = Button(app, text='Errado', width=10)
+b2.pack(side='right', padx=10, pady=10)
 
 app.mainloop()
